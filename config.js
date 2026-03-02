7242b7846a51829c733a1566ec1f8dba8f0ed6d1
reissondesigner-lang <reissondesigner@gmail.com>
2026-03-01 22:36:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,10 @@
-// 🔐 SUA CHAVE FIXA AQUI
-export const GEMINI_KEY = "AIzaSyCfvDdS_kU4hTeBvSoF845BfeMIaSGh4YI";
+// config.js
+
+// Primeira parte da chave
+export const GEMINI_KEY_PART1 = "AIzaSyA466PqsBs8AVN";
+
+// Segunda parte da chave
+export const GEMINI_KEY_PART2 = "nh66CudCpUU8Cpxsn7YA";
+
+// Concatenar as partes para formar a chave completa
+export const GEMINI_KEY = GEMINI_KEY_PART1 + GEMINI_KEY_PART2;
